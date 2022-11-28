467b78d3b03f8323871e634ec2053968906927f5
Sven-Hendrik Haase <svenstaro@gmail.com>
2022-11-28 15:54:57 +0100

    Use openraft example as starting point

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use anyhow::Result;
 
+mod node;
+mod raft_network;
+mod store;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Hello, world!");
--- src/node.rs
+++ src/node.rs
@@ -1,3 +1,57 @@
-use anyhow::Result;
+use std::sync::Arc;
 
-pub async fn start_node(node_id: u64) -> Result<()> {}
+use anyhow::Result;
+use openraft::BasicNode;
+use openraft::Config;
+use openraft::Raft;
+
+use crate::raft_network::ExampleNetwork;
+use crate::store::ExampleRequest;
+use crate::store::ExampleResponse;
+use crate::store::ExampleStore;
+
+pub type ExampleNodeId = u64;
+
+openraft::declare_raft_types!(
+    /// Declare the type configuration for example K/V store.
+    pub ExampleTypeConfig: D = ExampleRequest, R = ExampleResponse, NodeId = ExampleNodeId, Node = BasicNode
+);
+
+pub type ExampleRaft = Raft<ExampleTypeConfig, ExampleNetwork, Arc<ExampleStore>>;
+
+// Representation of an application state. This struct can be shared around to share
+// instances of raft, store and more.
+pub struct ExampleApp {
+    pub id: ExampleNodeId,
+    pub addr: String,
+    pub raft: ExampleRaft,
+    pub store: Arc<ExampleStore>,
+    pub config: Arc<Config>,
+}
+
+pub async fn start_node(node_id: u64) -> Result<()> {
+    // Create a configuration for the raft instance.
+    let config = Arc::new(Config::default().validate().unwrap());
+
+    // Create a instance of where the Raft data will be stored.
+    let store = Arc::new(ExampleStore::default());
+
+    // Create the network layer that will connect and communicate the raft instances and
+    // will be used in conjunction with the store created above.
+    let network = ExampleNetwork {};
+
+    // Create a local raft instance.
+    let raft = Raft::new(node_id, config.clone(), network, store.clone());
+
+    // Create an application that will store all the instances created above, this will
+    // be later used on the actix-web services.
+    // let app = Data::new(ExampleApp {
+    //     id: node_id,
+    //     addr: http_addr.clone(),
+    //     raft,
+    //     store,
+    //     config,
+    // });
+
+    Ok(())
+}
