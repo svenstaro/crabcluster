4bbc8c48bbb7f29ac2bcaa8a86e196a90b9dfbbf
Sven-Hendrik Haase <svenstaro@gmail.com>
2022-11-30 19:19:54 +0100

    Rename write/read to kv_write/kv_read

--- src/network/user_api.rs
+++ src/network/user_api.rs
@@ -5,7 +5,7 @@ use openraft::error::Infallible;
 
 use crate::{node::RaftApp, store::RaftRequest};
 
-pub async fn write(
+pub async fn kv_write(
     State(app_state): State<RaftApp>,
     Json(req): Json<RaftRequest>,
 ) -> impl IntoResponse {
@@ -13,7 +13,10 @@ pub async fn write(
     (StatusCode::OK, Json(res))
 }
 
-pub async fn read(State(app_state): State<RaftApp>, Json(req): Json<String>) -> impl IntoResponse {
+pub async fn kv_read(
+    State(app_state): State<RaftApp>,
+    Json(req): Json<String>,
+) -> impl IntoResponse {
     let state_machine = app_state.store.state_machine.read().await;
     let key = req;
     let value = state_machine.data.get(&key).cloned();
